f9a6bce8e62bdeffc67a41714c79788581239d07
Samarth Vishwakarma <139889802+Alwaysamarth@users.noreply.github.com>
2024-11-11 21:16:13 +0530

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Menu, X, Brain } from 'lucide-react';
-import Container from './common/Container';
+import Container from './Container';
 
 const navigation = [
   { name: 'Home', href: '#' },
